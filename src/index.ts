98a1f0c90d15d59c860e13d60e9076c74bedbe7a
Bobby Gifford <bobby077joe@gmail.com>
2024-12-03 15:03:08 -0800

    frontend boiler plate

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
-import express, { type Request, type Response } from 'express';
-import { registerValidation } from './dto/dto.ts';
-import { loginHandler, protectedRouteHandler, registerHandler } from "./handlers/auth.ts";
+import express, { type Request, type Response } from "express";
+import { registerValidation } from "./dto/dto.ts";
+import {
+  loginHandler,
+  protectedRouteHandler,
+  registerHandler,
+} from "./handlers/auth.ts";
 import { authMiddleware } from "./middleware/auth.ts";
-import cookieParser from 'cookie-parser';
+import cookieParser from "cookie-parser";
 import { createBuilderPost, getBuilderPost } from "./handlers/builderPosts.ts";
 
 const app = express();
@@ -11,34 +15,34 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/register',
-    registerValidation,
-    // @ts-ignore
-    registerHandler
+app.post(
+  "/register",
+  registerValidation,
+  // @ts-ignore
+  registerHandler
 );
 
-app.post('/builder_posts',
-    // @ts-ignore
-    authMiddleware,
-    createBuilderPost);
+app.post(
+  "/builder_posts",
+  // @ts-ignore
+  authMiddleware,
+  createBuilderPost
+);
 
 app.get("/builder_posts", getBuilderPost);
 
-app.post('/login',
-    loginHandler
-);
+app.post("/login", loginHandler);
 
 // Protected route example
 // @ts-ignore
-app.get('/protected', authMiddleware, protectedRouteHandler);
+app.get("/protected", authMiddleware, protectedRouteHandler);
 // Logout route
-app.post('/logout', (_req: Request, res: Response) => {
-    res.clearCookie('jwt');
-    res.json({ message: 'Logged out successfully' });
+app.post("/logout", (_req: Request, res: Response) => {
+  res.clearCookie("jwt");
+  res.json({ message: "Logged out successfully" });
 });
 
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
-
